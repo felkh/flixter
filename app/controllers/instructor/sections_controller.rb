cec1a0fb12b1c5756a922d5ef6bd9ca835855e4a
Felisa <felisa.kh@hotmail.com>
2019-05-21 22:42:11 +0000

    moved new section form into modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,10 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:create]
-
-  def new
-    @section = Section.new
-  end
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def create
     @section = current_course.sections.create(section_params)
