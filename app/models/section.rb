62bb2607672af7d4bb7e3877a92338cefd259de9
Felisa <felisa.kh@hotmail.com>
2019-05-22 03:12:07 +0000

    fixed video uploading issue

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,12 +5,8 @@ class Section < ApplicationRecord
   include RankedModel
   ranks :row_order, with_same: :course_id
 
-  def next_lesson
-    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
-    if lesson.blank? && section.next_section
-      return section.next_section.lessons.rank(:row_order).first
-    end
-    return lesson
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
   end
-
 end
