92ff2f5cba6d97a770005660cc84c6c4eced1c74
Felisa <felisa.kh@hotmail.com>
2019-05-22 01:38:44 +0000

    added static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,13 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def privacy
+  end
+
+  def team
+  end
+
+  def careers
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'careers', to: 'static_pages#careers'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
